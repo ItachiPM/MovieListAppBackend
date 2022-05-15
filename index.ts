9cc875f8575eb378e68eef36b9f640f1da55768f
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-05-15 10:49:09 +0200

    Added global handling error

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as cors from 'cors';
 import {genreRouter} from "./routers/genre";
 import {movieRouter} from "./routers/movie";
+import {handleError} from "./utils/error";
 
 const app = express()
 
@@ -12,6 +13,8 @@ app.use(express.json());
 
 app.use('/genre', genreRouter)
 app.use('/movie', movieRouter)
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on http://localhost:3001');
