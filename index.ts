df03e03247357bb40f2fffd32d8805906f4cf9d6
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-05-28 13:21:21 +0200

    fixed Error handling

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import * as express from 'express';
+import 'express-async-errors' ;
 import * as cors from 'cors';
 import {genreRouter} from "./routers/genre";
 import {movieRouter} from "./routers/movie";
+import {handleError} from "./utils/error";
 
 const app = express()
 
@@ -12,6 +14,8 @@ app.use(express.json());
 
 app.use('/genre', genreRouter)
 app.use('/movie', movieRouter)
+
+app.use(handleError)
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on http://localhost:3001');
